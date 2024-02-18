d11342641afd3c85441f961db0888b8a6b275fd5
AbdUllah_Aiman <abdullah.aiman.elsheshtawy@gmail.com>
2024-02-18 20:30:51 +0200

    Added (Admin-Class-StidentView) Forms

---
+++ AttendanceSysytem/Forms/StudentEditForm.Designer.cs
@@ -0,0 +1,220 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace AttendanceSysytem.Forms
+{
+    partial class StudentEditForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.edit_student_label = new System.Windows.Forms.Label();
+            this.name_label = new System.Windows.Forms.Label();
+            this.email_label = new System.Windows.Forms.Label();
+            this.password_label = new System.Windows.Forms.Label();
+            this.class_label = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.delete_student_btn = new System.Windows.Forms.Button();
+            this.save_btn = new System.Windows.Forms.Button();
+            this.cancel_btn = new System.Windows.Forms.Button();
+            this.classes_list = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // edit_student_label
+            // 
+            this.edit_student_label.AutoSize = true;
+            this.edit_student_label.Enabled = false;
+            this.edit_student_label.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.edit_student_label.Location = new System.Drawing.Point(309, 44);
+            this.edit_student_label.Name = "edit_student_label";
+            this.edit_student_label.Size = new System.Drawing.Size(203, 46);
+            this.edit_student_label.TabIndex = 0;
+            this.edit_student_label.Text = "Edit Student";
+            // 
+            // name_label
+            // 
+            this.name_label.AutoSize = true;
+            this.name_label.Enabled = false;
+            this.name_label.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.name_label.Location = new System.Drawing.Point(119, 132);
+            this.name_label.Name = "name_label";
+            this.name_label.Size = new System.Drawing.Size(78, 32);
+            this.name_label.TabIndex = 1;
+            this.name_label.Text = "Name";
+            // 
+            // email_label
+            // 
+            this.email_label.AutoSize = true;
+            this.email_label.Enabled = false;
+            this.email_label.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.email_label.Location = new System.Drawing.Point(116, 184);
+            this.email_label.Name = "email_label";
+            this.email_label.Size = new System.Drawing.Size(81, 32);
+            this.email_label.TabIndex = 2;
+            this.email_label.Text = "E-mail";
+            // 
+            // password_label
+            // 
+            this.password_label.AutoSize = true;
+            this.password_label.Enabled = false;
+            this.password_label.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.password_label.Location = new System.Drawing.Point(116, 232);
+            this.password_label.Name = "password_label";
+            this.password_label.Size = new System.Drawing.Size(111, 32);
+            this.password_label.TabIndex = 3;
+            this.password_label.Text = "Password";
+            // 
+            // class_label
+            // 
+            this.class_label.AutoSize = true;
+            this.class_label.Enabled = false;
+            this.class_label.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.class_label.Location = new System.Drawing.Point(119, 284);
+            this.class_label.Name = "class_label";
+            this.class_label.Size = new System.Drawing.Size(67, 32);
+            this.class_label.TabIndex = 4;
+            this.class_label.Text = "Class";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.textBox1.Location = new System.Drawing.Point(262, 129);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(335, 39);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.textBox2.Location = new System.Drawing.Point(262, 181);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(335, 39);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.textBox3.Location = new System.Drawing.Point(262, 229);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(335, 39);
+            this.textBox3.TabIndex = 7;
+            // 
+            // delete_student_btn
+            // 
+            this.delete_student_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.delete_student_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.delete_student_btn.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.delete_student_btn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.delete_student_btn.Location = new System.Drawing.Point(70, 380);
+            this.delete_student_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.delete_student_btn.Name = "delete_student_btn";
+            this.delete_student_btn.Size = new System.Drawing.Size(229, 43);
+            this.delete_student_btn.TabIndex = 9;
+            this.delete_student_btn.Text = "Delete Student";
+            this.delete_student_btn.UseVisualStyleBackColor = false;
+            // 
+            // save_btn
+            // 
+            this.save_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.save_btn.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.save_btn.Location = new System.Drawing.Point(495, 379);
+            this.save_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.save_btn.Name = "save_btn";
+            this.save_btn.Size = new System.Drawing.Size(102, 44);
+            this.save_btn.TabIndex = 10;
+            this.save_btn.Text = "Save";
+            this.save_btn.UseVisualStyleBackColor = true;
+            // 
+            // cancel_btn
+            // 
+            this.cancel_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cancel_btn.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.cancel_btn.Location = new System.Drawing.Point(631, 379);
+            this.cancel_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cancel_btn.Name = "cancel_btn";
+            this.cancel_btn.Size = new System.Drawing.Size(102, 44);
+            this.cancel_btn.TabIndex = 11;
+            this.cancel_btn.Text = "Cancel";
+            this.cancel_btn.UseVisualStyleBackColor = true;
+            // 
+            // classes_list
+            // 
+            this.classes_list.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.classes_list.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.classes_list.FormattingEnabled = true;
+            this.classes_list.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.classes_list.Location = new System.Drawing.Point(262, 281);
+            this.classes_list.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.classes_list.Name = "classes_list";
+            this.classes_list.Size = new System.Drawing.Size(335, 39);
+            this.classes_list.TabIndex = 25;
+            // 
+            // StudentEditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 464);
+            this.Controls.Add(this.classes_list);
+            this.Controls.Add(this.cancel_btn);
+            this.Controls.Add(this.save_btn);
+            this.Controls.Add(this.delete_student_btn);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.class_label);
+            this.Controls.Add(this.password_label);
+            this.Controls.Add(this.email_label);
+            this.Controls.Add(this.name_label);
+            this.Controls.Add(this.edit_student_label);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "StudentEditForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label edit_student_label;
+        private Label name_label;
+        private Label email_label;
+        private Label password_label;
+        private Label class_label;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button delete_student_btn;
+        private Button save_btn;
+        private Button cancel_btn;
+        private ComboBox classes_list;
+    }
+}
---
+++ AttendanceSysytem/Forms/StudentForm.Designer.cs
@@ -0,0 +1,265 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace AttendanceSysytem.Forms
+{
+    partial class StudentForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Day = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DATE = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.STATUS = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(169, 29);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(187, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Class";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(169, 67);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(187, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Show From";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(352, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "To";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(169, 110);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 24);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(417, 110);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(151, 24);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Day,
+            this.DATE,
+            this.STATUS});
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.dataGridView1.Location = new System.Drawing.Point(60, 184);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(582, 150);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(674, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Absent Days";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(765, 107);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(44, 22);
+            this.textBox3.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(765, 398);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 42);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Sign out";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(629, 398);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 42);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Save As";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Day
+            // 
+            this.Day.HeaderText = "DAY";
+            this.Day.MinimumWidth = 6;
+            this.Day.Name = "Day";
+            this.Day.ReadOnly = true;
+            this.Day.Width = 125;
+            // 
+            // DATE
+            // 
+            this.DATE.HeaderText = "DATE";
+            this.DATE.MinimumWidth = 6;
+            this.DATE.Name = "DATE";
+            this.DATE.ReadOnly = true;
+            this.DATE.Width = 125;
+            // 
+            // STATUS
+            // 
+            this.STATUS.HeaderText = "STATUS";
+            this.STATUS.MinimumWidth = 6;
+            this.STATUS.Name = "STATUS";
+            this.STATUS.ReadOnly = true;
+            this.STATUS.Width = 125;
+            // 
+            // StudentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 499);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "StudentForm";
+            this.Text = "Student";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label4;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private DataGridView dataGridView1;
+        private Label label5;
+        private TextBox textBox3;
+        private Button button1;
+        private Button button2;
+        private DataGridViewTextBoxColumn Day;
+        private DataGridViewTextBoxColumn DATE;
+        private DataGridViewTextBoxColumn STATUS;
+    }
+}
