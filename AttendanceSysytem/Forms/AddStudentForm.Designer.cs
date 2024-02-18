940b69a78bdd35c6c2ab5f8ef36da82d3177f29d
omarabdallah10 <omarabdallah716@yahoo.com>
2024-02-18 15:55:13 +0200

    Structure added

---
+++ AttendanceSysytem/Forms/AddStudentForm.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace AttendanceSysytem
+{
+    partial class AddStudentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.Label();
+            this.Username = new System.Windows.Forms.Label();
+            this.txtClass = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.Class = new System.Windows.Forms.Label();
+            this.Name1 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.AddStudent = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(221, 304);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(175, 35);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(364, 163);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(189, 22);
+            this.txtPassword.TabIndex = 8;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(364, 111);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(189, 22);
+            this.txtUserName.TabIndex = 7;
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Location = new System.Drawing.Point(247, 166);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(67, 16);
+            this.Password.TabIndex = 6;
+            this.Password.Text = "Password";
+            // 
+            // Username
+            // 
+            this.Username.AutoSize = true;
+            this.Username.Location = new System.Drawing.Point(247, 114);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(70, 16);
+            this.Username.TabIndex = 5;
+            this.Username.Text = "Username";
+            // 
+            // txtClass
+            // 
+            this.txtClass.Location = new System.Drawing.Point(364, 219);
+            this.txtClass.Name = "txtClass";
+            this.txtClass.Size = new System.Drawing.Size(189, 22);
+            this.txtClass.TabIndex = 13;
+            this.txtClass.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(364, 68);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(189, 22);
+            this.txtName.TabIndex = 12;
+            this.txtName.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Class
+            // 
+            this.Class.AutoSize = true;
+            this.Class.Location = new System.Drawing.Point(247, 222);
+            this.Class.Name = "Class";
+            this.Class.Size = new System.Drawing.Size(41, 16);
+            this.Class.TabIndex = 11;
+            this.Class.Text = "Class";
+            this.Class.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Name
+            // 
+            this.Name1.AutoSize = true;
+            this.Name1.Location = new System.Drawing.Point(247, 71);
+            this.Name1.Name = "Name";
+            this.Name1.Size = new System.Drawing.Size(44, 16);
+            this.Name1.TabIndex = 10;
+            this.Name1.Text = "Name";
+            this.Name1.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(470, 304);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(175, 35);
+            this.btnCancel.TabIndex = 14;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // AddStudent
+            // 
+            this.AddStudent.AutoSize = true;
+            this.AddStudent.Location = new System.Drawing.Point(382, 26);
+            this.AddStudent.Name = "AddStudent";
+            this.AddStudent.Size = new System.Drawing.Size(80, 16);
+            this.AddStudent.TabIndex = 15;
+            this.AddStudent.Text = "Add Student";
+            // 
+            // AddStudentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AddStudent);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.txtClass);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.Class);
+            this.Controls.Add(this.Name1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Username);
+            this.Name = "AddStudentForm";
+            this.Text = "AddStudentForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.Label Username;
+        private System.Windows.Forms.TextBox txtClass;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label Class;
+        private System.Windows.Forms.Label Name1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label AddStudent;
+    }
+}
