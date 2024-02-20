9faf7a1d3a505e9f85f2e85dabe8aa19fc47b3d9
omarabdallah10 <omarabdallah716@yahoo.com>
2024-02-20 23:32:11 +0200

    View Attendance added dynamically beside XML edit

--- AttendanceSysytem/Forms/TeacherViewAttendanceForm.Designer.cs
+++ AttendanceSysytem/Forms/TeacherViewAttendanceForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AttendanceSysytem.Forms
 {
-    partial class TeacherViewForm
+    partial class TeacherViewAttendanceForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,27 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewAttendance = new System.Windows.Forms.DataGridView();
             this.btnGoBack = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.ClassesCompoBox = new System.Windows.Forms.ComboBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.TrackCompoBox = new System.Windows.Forms.ComboBox();
+            this.DateComboBox = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.btnViewAttendance = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAttendance)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dataGridViewAttendance
             // 
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-            this.dataGridView1.Location = new System.Drawing.Point(26, 186);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(777, 242);
-            this.dataGridView1.TabIndex = 0;
+            this.dataGridViewAttendance.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.dataGridViewAttendance.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewAttendance.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridViewAttendance.Location = new System.Drawing.Point(26, 186);
+            this.dataGridViewAttendance.Name = "dataGridViewAttendance";
+            this.dataGridViewAttendance.RowHeadersWidth = 51;
+            this.dataGridViewAttendance.RowTemplate.Height = 24;
+            this.dataGridViewAttendance.Size = new System.Drawing.Size(777, 242);
+            this.dataGridViewAttendance.TabIndex = 6;
             // 
             // btnGoBack
             // 
@@ -59,7 +59,7 @@
             this.btnGoBack.Location = new System.Drawing.Point(41, 23);
             this.btnGoBack.Name = "btnGoBack";
             this.btnGoBack.Size = new System.Drawing.Size(200, 40);
-            this.btnGoBack.TabIndex = 1;
+            this.btnGoBack.TabIndex = 5;
             this.btnGoBack.Text = "Go Back";
             this.btnGoBack.UseVisualStyleBackColor = false;
             // 
@@ -70,56 +70,37 @@
             this.label1.Location = new System.Drawing.Point(398, 38);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(142, 25);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Class Name: ";
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Track Name: ";
             // 
-            // ClassesCompoBox
+            // TrackCompoBox
             // 
-            this.ClassesCompoBox.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.ClassesCompoBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ClassesCompoBox.FormattingEnabled = true;
-            this.ClassesCompoBox.Items.AddRange(new object[] {
+            this.TrackCompoBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.TrackCompoBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TrackCompoBox.FormattingEnabled = true;
+            this.TrackCompoBox.Items.AddRange(new object[] {
             "PD",
             "OS",
             "AI"});
-            this.ClassesCompoBox.Location = new System.Drawing.Point(546, 35);
-            this.ClassesCompoBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.ClassesCompoBox.Name = "ClassesCompoBox";
-            this.ClassesCompoBox.Size = new System.Drawing.Size(200, 28);
-            this.ClassesCompoBox.TabIndex = 9;
+            this.TrackCompoBox.Location = new System.Drawing.Point(546, 35);
+            this.TrackCompoBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TrackCompoBox.Name = "TrackCompoBox";
+            this.TrackCompoBox.Size = new System.Drawing.Size(200, 28);
+            this.TrackCompoBox.TabIndex = 1;
             // 
-            // comboBox1
+            // DateComboBox
             // 
-            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.ItemHeight = 20;
-            this.comboBox1.Items.AddRange(new object[] {
-            "1/1/2024",
-            "2/1/2014",
-            "3/1/2024",
-            "4/1/2014",
-            "5/1/2024",
-            "6/1/2014",
-            "7/1/2024",
-            "8/1/2014",
-            "9/1/2024",
-            "10/1/2024",
-            "11/1/2024",
-            "12/1/2014",
-            "13/1/2024",
-            "14/1/2014",
-            "15/1/2024",
-            "16/1/2014",
-            "17/1/2024",
-            "18/1/2014",
-            "19/1/2024",
-            "20/1/2024"});
-            this.comboBox1.Location = new System.Drawing.Point(546, 79);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(200, 28);
-            this.comboBox1.TabIndex = 11;
+            this.DateComboBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.DateComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DateComboBox.FormattingEnabled = true;
+            this.DateComboBox.ItemHeight = 20;
+            this.DateComboBox.Items.AddRange(new object[] {
+            ""});
+            this.DateComboBox.Location = new System.Drawing.Point(546, 79);
+            this.DateComboBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.DateComboBox.Name = "DateComboBox";
+            this.DateComboBox.Size = new System.Drawing.Size(200, 28);
+            this.DateComboBox.TabIndex = 3;
             // 
             // label2
             // 
@@ -128,7 +109,7 @@
             this.label2.Location = new System.Drawing.Point(398, 82);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(70, 25);
-            this.label2.TabIndex = 10;
+            this.label2.TabIndex = 2;
             this.label2.Text = "Date: ";
             // 
             // btnViewAttendance
@@ -139,27 +120,28 @@
             this.btnViewAttendance.Location = new System.Drawing.Point(428, 130);
             this.btnViewAttendance.Name = "btnViewAttendance";
             this.btnViewAttendance.Size = new System.Drawing.Size(280, 40);
-            this.btnViewAttendance.TabIndex = 12;
+            this.btnViewAttendance.TabIndex = 4;
             this.btnViewAttendance.Text = "View Attendance";
             this.btnViewAttendance.UseVisualStyleBackColor = false;
-            this.btnViewAttendance.Click += new System.EventHandler(this.btnShowAttendance_Click);
+            this.btnViewAttendance.Click += new System.EventHandler(this.btnViewAttendance_Click);
             // 
-            // TeacherViewForm
+            // TeacherViewAttendanceForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.ClientSize = new System.Drawing.Size(832, 453);
             this.Controls.Add(this.btnViewAttendance);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.DateComboBox);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.ClassesCompoBox);
+            this.Controls.Add(this.TrackCompoBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnGoBack);
-            this.Controls.Add(this.dataGridView1);
-            this.Name = "TeacherViewForm";
+            this.Controls.Add(this.dataGridViewAttendance);
+            this.Name = "TeacherViewAttendanceForm";
             this.Text = "TeacherView";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.TeacherViewAttendanceForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAttendance)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -167,11 +149,11 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridViewAttendance;
         private System.Windows.Forms.Button btnGoBack;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox ClassesCompoBox;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox TrackCompoBox;
+        private System.Windows.Forms.ComboBox DateComboBox;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnViewAttendance;
     }
--- AttendanceSysytem/Program.cs
+++ AttendanceSysytem/Program.cs
@@ -22,7 +22,7 @@ namespace AttendanceSysytem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TakeAttendanceForm());
+            Application.Run(new TeacherViewAttendanceForm());
 
         }
     }
