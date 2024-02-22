1d8c9953d4ce6de981e79f169c09466440ef6468
omarabdallah10 <omarabdallah716@yahoo.com>
2024-02-22 23:24:40 +0200

    Some enhancements on View Attendance -- Take attendance V1 is added

--- AttendanceSysytem/Forms/TakeAttendanceForm.cs
+++ AttendanceSysytem/Forms/TakeAttendanceForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AttendanceSysytem.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace AttendanceSysytem.Forms
 {
@@ -17,8 +19,58 @@ namespace AttendanceSysytem.Forms
             InitializeComponent();
         }
 
-        private void btnViewAttendance_Click(object sender, EventArgs e)
+
+        
+        private void TakeAttendanceForm_Load(object sender, EventArgs e)
         {
+            //'select track' is displayed by default
+            ClassComboBox.Text = "Select Class";
+
+            dataGridViewTakeAttendance.AllowUserToAddRows = false;
+            
+
+        }
+
+        private void ClassComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //clear the datagridview
+            dataGridViewTakeAttendance.Columns.Clear();
+
+            //display the students of the selected class from the combobox in the datagridview
+            try
+            {
+                XmlDataDocument xmlData = new XmlDataDocument();
+                string xmlPath = DataManagement.xmlPath();
+                xmlData.DataSet.ReadXml(xmlPath);
+
+                //filtering the students of the selected class according to the class name
+                DataView dv = new DataView(xmlData.DataSet.Tables["Student"]);
+                dv.RowFilter = "ClassName = '" + ClassComboBox.Text + "'";
+
+                //display the name of the user id and the name of the students in the datagridview
+                DataTable dc = dv.ToTable(true, "UserID", "Name");
+                dataGridViewTakeAttendance.DataSource = dc;
+
+                // add a checkbox column to the datagridview
+                DataGridViewCheckBoxColumn checkColumn = new DataGridViewCheckBoxColumn();
+                checkColumn.Name = "AttendanceStatusCheckBox";
+                checkColumn.HeaderText = "Status";
+                checkColumn.Width = 100;
+                checkColumn.ReadOnly = false;
+                
+                /*checkColumn.TrueValue = true;
+                checkColumn.FalseValue = false;*/
+                dataGridViewTakeAttendance.Columns.Add(checkColumn);
+
+                
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something went wrong!! please try again later");
+            }
+
 
         }
     }
