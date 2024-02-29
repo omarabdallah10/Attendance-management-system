74aa27216beb2538f13216f57d0e29a3a511fb27
your_username <abdooaiman@std.mans.edu.eg>
2024-02-29 23:37:57 +0200

    Added View Attendance

--- AttendanceSysytem/Forms/teacherFunctionalitiesForm.cs
+++ AttendanceSysytem/Forms/teacherFunctionalitiesForm.cs
@@ -31,7 +31,7 @@ namespace AttendanceSysytem
 
         private void btnViewAttendance_Click(object sender, EventArgs e)
         {
-            TeacherViewAttendanceForm form = new TeacherViewAttendanceForm();
+            view_attendance form = new view_attendance();
             form.recived = recived;
             form.Show();
             Hide();
